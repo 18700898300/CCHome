2c2b067dd8af7c973a26aedbb28a49ce27f74d2e
18700898300 <781175983@qq.com>
2017-11-27 09:16:42 +0800

    update CCHome to shouye

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CCHome
                 </div>
 
                 <div class="links">
